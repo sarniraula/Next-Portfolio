c9a776d4ea0e30d5a413097fda2759c9989d5d0c
sarniraula <sar.niraula162@gmail.com>
2024-06-29 12:05:03 -0400

    Added a 3D model and fixed lightings.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,29 +1,35 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useEffect, useState, } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./lake/scene.gltf");
+  const meshRef = React.useRef();
+
+  useFrame((state, delta) => {
+    // Rotate the model slowly around the z-axis
+    meshRef.current.rotation.y += delta * 0.1; // Adjust the speed as needed
+  });
 
   return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+    <mesh ref={meshRef}>
+      <hemisphereLight intensity={1} groundColor='black' />
       <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
+        position={[0,0,-4]}
+        angle={0}
         penumbra={1}
         intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight position={[0,0,-4]} intensity={5} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.7 : 0.3}
+        position={isMobile ? [0, -3, -2.2] : [0, 0, -4]}
+        rotation={[0, 0, 0]}
       />
     </mesh>
   );
@@ -64,8 +70,8 @@ const ComputersCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2.5}
+          minPolarAngle={Math.PI / 2.5}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
